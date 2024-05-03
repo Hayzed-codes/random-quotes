27080607b125a9dd16023f37a40d45c79c2a5624
Hayzed-codes <raheemhorlah@gmail.com>
2024-05-03 22:19:40 +0100

    Random-quote

---
+++ main.js
@@ -0,0 +1,84 @@
+// <----------Main.js---------->
+
+const quoteCon = document.getElementById('quote-container');
+
+const quoteText = document.getElementById('quote');
+
+const authorName = document.getElementById('author');
+
+const twitterBtn = document.getElementById('twitter');
+
+const newQuoteBtn = document.getElementById('new-quote');
+
+const loader = document.getElementById('loader');
+
+//https://type.fit/api/quotes
+let apiQuote = [];
+
+//Show Loading
+
+function loading() {
+    loader.hidden = false;
+    quoteCon.hidden = true;
+}
+
+//Hide Loading
+
+function complete(){
+    quoteCon.hidden = false;
+    loader.hidden = true;
+}
+
+
+
+function newQuote() {
+
+    loading()
+
+    const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
+    console.log(quote);
+
+    if(!quote.author) {
+        authorName.textContent = 'unknown'
+    }else {
+        authorName.textContent = quote.author
+    }
+
+    //Check quote length to determine the styling
+    if(quote.text.length > 50) {
+        quoteText.classList.add('long-quote')
+    }else {
+        quoteText.classList.remove('long-quote')  
+    } 
+    
+    quoteText.textContent = quote.text
+
+    complete()
+
+}
+
+async function getQuotes() {
+    loading()
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuote = await response.json()
+        console.log(apiQuote)
+
+        newQuote()
+    }catch(error) {
+        console.log(error)
+
+    }
+}
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}-${authorName.textContent}`
+    window.open(twitterUrl, "_blank")
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+getQuotes()
---
+++ script.js
@@ -0,0 +1,84 @@
+// <-------Script.js-------->
+
+const quoteCon = document.getElementById('quote-container');
+
+const quoteText = document.getElementById('quote');
+
+const authorName = document.getElementById('author');
+
+const twitterBtn = document.getElementById('twitter');
+
+const newQuoteBtn = document.getElementById('new-quote');
+
+const loader = document.getElementById('loader');
+
+//Get Quote API
+
+
+//https://api.quotable.io/random
+
+
+//Show Loading
+
+const showLoading = () => {
+    loader.hidden = false;
+    quoteCon.hidden = true;
+}
+
+//Hide Loading
+
+const hideLoading = () => {
+   if(!loader.hidden) {
+    quoteCon.hidden = false;
+    loader.hidden = true;
+   }
+}
+
+const getQuote = async() => {
+
+    showLoading()
+    const apiUrl = 'https://api.quotable.io/random';
+
+    try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        
+
+        if(data.author === '') {
+            authorName.innerText = 'unknown';
+        }else { authorName.innerText = data.author;
+        }
+
+         //Check quote length to determine the styling
+
+         if(data.content.length > 50) {
+            quoteText.classList.add('long-quote')
+        }else {
+            quoteText.classList.remove('long-quote')  
+        } 
+
+            
+
+        authorName.innerText = data.author;
+        quoteText.innerText = data.content;
+
+        hideLoading()
+    }catch(error) {
+        console.log('Error fetching quote', error)
+        getQuote()// retry fetching quote
+    }
+}
+
+//Tweet
+const tweetQuote = ()=> {
+    const quote = quoteText.innerText;
+    const author = authorName.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote}-${author}`
+    window.open(twitterUrl, "_blank")
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+    
+getQuote()
